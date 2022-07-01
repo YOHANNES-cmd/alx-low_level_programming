0d44a73b5d1a35a23faa527c3e55ff5ef4b7d81f
Yohannes <johanneskanko7@gmail.com>
2022-07-01 06:08:34 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * @void: Null value
+ *
+ * Description: print alphabet lowercase
+ * Return: zero value
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if(c == 'q' || c == 'e')
+			continue;
+		putchar(c);
+	}
+	putchar('\n');
+	retun (0);
+}
