0347e2f30e03a245fee4d935252c15e6b6c01998
Yohannes <johanneskanko7@gmail.com>
2022-07-01 05:20:50 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * Main- Entry point
+ * @void: Null value
+ *
+ * Description: print alphabet lowercase
+ * Return: zero value
+ */
+int main(void)
+{
+	char c, b;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (b = 'A'; b <= 'Z'; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
