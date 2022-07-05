b4b0ac91d13bad66ec3a4b517e48c56b48482fa9
Yohannes <johanneskanko7@gmail.com>
2022-07-05 06:34:09 -0700

    task 12 100 times tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -39,9 +39,9 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar((product / 100 + '0');
+					_putchar(product / 100 + '0');
 					_putchar((product / 10) % 10 + '0');
-					_putchar((product % 10 + '0');
+					_putchar(product % 10 + '0');
 						}
 						}
 						_putchar('\n');
