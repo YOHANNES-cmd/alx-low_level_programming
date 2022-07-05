4eb24a7a33c8a43279538da52da14e8849bae1fd
Yohannes <johanneskanko7@gmail.com>
2022-07-05 05:32:17 -0700

    task 8 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -25,7 +25,7 @@ void jack_bauer(void)
 			minutes_tens = '0';
 			while (minutes_tens < '6')
 			{
-				minutes_ones  = '0'
+				minutes_ones  = '0';
 				while (minutes_ones < 58)
 			{
 				_putchar(hours_tens);
