a060c53d9200e07900c8bb69473e7fe324db6f7e
YOHANNES-cmd <74503656+YOHANNES-cmd@users.noreply.github.com>
2022-07-07 15:41:26 +0300

    task 3 print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,16 @@
 #include "main.h"
-
 /**
- * print_numbers -> print numbers
- * Return: nothing
- */
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
 void print_numbers(void)
 {
-	int x;
-
-	for (x = 0; x < 10; x++)
-		_putchar(x + '0');
-	_putchar('\n');
+int number = 48;
+for (number = 48; number < 58; number++)
+{
+_putchar(number);
+}
+_putchar(10);
 }
