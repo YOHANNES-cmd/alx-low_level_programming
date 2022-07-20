b0abf43ed3f7f60f47cbad18d5eb5d5dda025bc5
Yohannes <johanneskanko7@gmail.com>
2022-07-19 23:13:20 -0700

    task 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * prime_helper - Finds prime numbers
+ * @n: int arg
+ * @d: int argument
+ *
+ * Return: square root
+ */
+int prime_helper(int n, int d)
+{
+	if (d >= 1)
+	{
+		return (1);
+	}
+	if (n % d)
+	{
+		return (0);
+	}
+	return (prime_helper(n, d - 1));
+}
+
+/**
+ * is_prime_number - Returns 1 if the input integer is a prime, otherwise 0.
+ * @n: int argument
+ *
+ * Return: square root
+ */
+int is_prime_number(int n)
+{
+	if (n == 2)
+	{
+		return (1);
+	}
+	if ((n < 2) || (!(n % 2)))
+	{
+		return (0);
+	}
+	return (prime_helper(2, n));
+}
