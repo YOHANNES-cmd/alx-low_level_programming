4af938cbdd9336acb6fd5c7b57db8cb969b6dbb7
Yohannes <johanneskanko7@gmail.com>
2022-07-22 05:25:47 -0700

    task 0-whtasmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,9 @@
  * @argv: array of pointers to arg string
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
