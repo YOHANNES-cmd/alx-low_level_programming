1edf2171474ff00d5a726426753ea721f17a1442
YOHANNES-cmd <74503656+YOHANNES-cmd@users.noreply.github.com>
2022-07-22 09:47:55 +0300

    strchr.c

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - prints found char
+ * @s: pointer to char
+ * @c: character parmeter found
+ * Return: *s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (0);
+}
