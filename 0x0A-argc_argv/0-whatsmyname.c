de67b351b727a361f87f23e521bfc830eff2ebab
Yohannes <johanneskanko7@gmail.com>
2022-07-22 05:15:46 -0700

    task 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: arg count
+ * @argv: array of pointers to arg string
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,6 @@
+ 0x0A. C - argc, argv
+
+TASKS
+
+
+
