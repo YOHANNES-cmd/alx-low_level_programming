83c62c36e36828dc1255c79b43c5afe803d54a1a
Yohannes <johanneskanko7@gmail.com>
2022-07-22 05:49:37 -0700

    task 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments it received
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
