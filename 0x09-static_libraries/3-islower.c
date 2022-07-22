af2b7719493a3a93daa55d0a083c2b7d04799ea3
YOHANNES-cmd <74503656+YOHANNES-cmd@users.noreply.github.com>
2022-07-22 10:06:23 +0300

    islower.c

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase
+ * @c: c is an ASCII character
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
