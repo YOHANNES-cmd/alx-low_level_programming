ce04a99d5266e638ee173a347a06ea40ecf5c943
YOHANNES-cmd <74503656+YOHANNES-cmd@users.noreply.github.com>
2022-07-22 09:57:32 +0300

    strlen.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - Function that returns the length of a string
+ *
+ * @s: this is the input string
+ *
+ * Return: Length of the string
+ */
+
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0' ; index++)
+		;
+	return (index);
+}
