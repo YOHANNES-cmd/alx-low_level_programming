c935330b3dcbd38ae62ca929ca10fd7baa47f5c8
YOHANNES-cmd <74503656+YOHANNES-cmd@users.noreply.github.com>
2022-07-28 20:00:39 +0300

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,6 +10,8 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
-int _atoi(const char *s);
-void print_int(unsigned long int n);
+void _is_zero(char *argv[]);
+char *_initialize_array(char *ar, int lar);
+int _checknum(char *argv[], int n);
+int main(int argc, char *argv[]);
 #endif
