37c69a4a6f8f011cd307fcaaa3f928720abe0a6d
YOHANNES-cmd <74503656+YOHANNES-cmd@users.noreply.github.com>
2022-08-03 14:35:10 +0300

    3-op_function.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,11 +1,13 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * op_add - calculates the sum of two integers
- * @a: first integer
- * @b: second integer
+ * op_add - ...
+ * @a: ...
+ * @b: ...
  *
- * Return: sum of a and b
+ * Return: ...
  */
 int op_add(int a, int b)
 {
@@ -13,11 +15,11 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - calculates the difference of two integers
- * @a: first integer
- * @b: second integer
+ * op_sub - ...
+ * @a: ...
+ * @b: ...
  *
- * Return: difference of a and b
+ * Return: ...
  */
 int op_sub(int a, int b)
 {
@@ -25,11 +27,11 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - calculates the product of two integers
- * @a: first integer
- * @b: second integer
+ * op_mul - ...
+ * @a: ...
+ * @b: ...
  *
- * Return: product of a and b
+ *  Return: ...
  */
 int op_mul(int a, int b)
 {
@@ -37,25 +39,37 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - calculates the division of two integers
- * @a: first integer
- * @b: second integer
+ * op_div - ...
+ * @a: ...
+ * @b: ...
  *
- * Return: result of the division of a and b
+ *  Return: ...
  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	return (a / b);
 }
 
 /**
- * op_mod - calculates the remainder of the division of two integers
- * @a: first integer
- * @b: second integer
+ * op_mod - ...
+ * @a: ...
+ * @b: ...
  *
- * Return: remainder of a divided b
+ * Return: ...
  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	return (a % b);
 }
