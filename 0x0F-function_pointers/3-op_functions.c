dead6f9c805c804503f28ff1df8da03c851cb04d
Yohannes <johanneskanko7@gmail.com>
2022-08-03 05:03:11 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,66 +1,43 @@
 #include "3-calc.h"
-<<<<<<< HEAD
-<<<<<<< HEAD
-#include <stdlib.h>
-#include <stdio.h>
 /**
- * op_add - ..
- * @a: ..
- * @b: ...
- * Return: ...
-=======
-=======
-#include <stdlib.h>
-#include <stdio.h>
->>>>>>> 37c69a4a6f8f011cd307fcaaa3f928720abe0a6d
-
-/**
- * op_add - ...
- * @a: ...
- * @b: ...
+ * op_add - adds two numb.
+ * @a: first numb
+ * @b: second numb.
  *
-<<<<<<< HEAD
- * Return: sum of a and b
->>>>>>> 8ea2bb6b273b930ff1b58b15e0d29a3c42fe3bab
-=======
- * Return: ...
->>>>>>> 37c69a4a6f8f011cd307fcaaa3f928720abe0a6d
+ * Return: result.
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
- * op_sub - ...
- * @a: ...
- * @b: ...
+ * op_sub - subtract two numbers.
+ * @a: first numb.
+ * @b: second numb
  *
- * Return: ...
+ * Return: result.
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
- * op_mul - ...
- * @a: ...
- * @b: ...
+ * op_mul - multiplies two numb.
+ * @a: first numb.
+ * @b: second numb.
  *
- *  Return: ...
+ * Return: result.
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
- * op_div - ...
- * @a: ...
- * @b: ...
+ * op_div - divids two numb.
+ * @a: first numb.
+ * @b: second numb.
  *
- *  Return: ...
+ * Return: result.
  */
 int op_div(int a, int b)
 {
@@ -69,33 +46,22 @@ int op_div(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-
 	return (a / b);
 }
-
 /**
- * op_mod - ...
- * @a: ...
- * @b: ...
+ * op_mod - calculates the module of two numbers.
+ * @a: first numb.
+ * @b: second numb.
  *
- * Return: ...
+ * Return: reminder
  */
+
 int op_mod(int a, int b)
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 37c69a4a6f8f011cd307fcaaa3f928720abe0a6d
 	if (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-<<<<<<< HEAD
-=======
->>>>>>> 8ea2bb6b273b930ff1b58b15e0d29a3c42fe3bab
-=======
-
->>>>>>> 37c69a4a6f8f011cd307fcaaa3f928720abe0a6d
 	return (a % b);
 }
