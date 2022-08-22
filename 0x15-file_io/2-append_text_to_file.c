17d6fe03117abc313f1e8ac7a35ac2653303b237
Yohannes <johanneskanko7@gmail.com>
2022-08-22 04:18:33 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,29 +1,44 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: the name of the file
- * @text_content: the NULL terminated string to add at end of file
- * Return: 1 on success, -1 on failure
+ * append_text_to_file - Appends text at the end of a file
+ *
+ * @filename: Const char pointer
+ *
+ * @text_content: Char pointer
+ *
+ * Return: int
  */
+
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int file, fwrite, i;
+	int fdOpen, fdWrite, length = 0;
 
-    if (filename == NULL)
-        return (-1);
-    file = open(filename, O_APPEND | O_RDWR, 0664);
-    if (file == -1)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        for (i = 0; text_content[i]; i++)
-            ;
-        fwrite = write(file, text_content, i);
-        if (fwrite == -1)
-            return (-1);
-    }
-    close(file);
-    return (1);
+	fdOpen = open(filename, O_WRONLY | O_APPEND);
+
+	if (fdOpen == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[length] != '\0')
+	{
+		length++;
+	}
+
+	fdWrite = write(fdOpen, text_content, length);
+
+	if (fdWrite == -1)
+		return (-1);
+
+	close(fdOpen);
+
+	return (1);
 }
