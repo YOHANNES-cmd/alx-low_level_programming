125db19b3bc6662771e8e1159eb444ec46144572
YOHANNES KANKO <johanneskanko7@gmail.com>
2022-10-03 08:09:42 -0700

    last update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/YOHANNES-cmd/alx-low_level_programming
-/blob/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://github.com/YOHANNES-cmd/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
