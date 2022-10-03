e21ae93bbd2f125388a1798f02aafb6037ede32a
YOHANNES-cmd <74503656+YOHANNES-cmd@users.noreply.github.com>
2022-10-03 17:58:48 +0300

    101-make_me_win.sh update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/YOHANNES-cmd/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://raw.github.com/YOHANNES-cmd/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
